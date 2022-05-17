830c8827659ce92bcf9d0a62255bce7973a5e25c
vtenpo <phongzed2001@gmail.com>
2022-05-18 00:00:06 +0700

    commit

--- algorithms/sorting/insertion_sort.c
+++ algorithms/sorting/insertion_sort.c
@@ -32,7 +32,7 @@ void insertionSort(int arr[], int size) {
  * @param size size of array
  */
 void printArray(int arr[], int size) {
-    printf("Sorted array:\n")
+    printf("Sorted array:\n");
     for (int i = 0; i < size; i++) {
         printf("%d ", arr[i]);
     }
--- algorithms/sorting/selection_sort.c
+++ algorithms/sorting/selection_sort.c
@@ -48,7 +48,7 @@ void selectionSort(int arr[], int size) {
  * @param size size of array
  */
 void printArray(int arr[], int size) {
-    printf("Sorted array:\n")
+    printf("Sorted array:\n");
     for (int i = 0; i < size; i++) {
         printf("%d ", arr[i]);
     }
